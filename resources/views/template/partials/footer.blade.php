a43094d3f79e1a02c8d7d6d06d036386a18ed8f0
Sadar Khan <sadarkhan@abtach.org>
2019-07-16 19:13:00 +0500

    footer style change

--- resources/views/template/partials/footer.blade.php
+++ resources/views/template/partials/footer.blade.php
@@ -1,4 +1,4 @@
-            <footer class="sticky-footer">
+            <footer class="sticky-footer" style="  position: relative; bottom: 0;  width: 100%; height: 50px;">
                 <div class="container my-auto">
                     <div class="copyright text-center my-auto">
                         <span>Copyright © SCMS - <?=Date('Y')?></span>
