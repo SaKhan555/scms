7947ae9381730f5689d989aee1a3a9509244d929
Sadar Khan <sadarkhan@abtach.org>
2019-07-17 14:56:29 +0500

    make changes in master js function

--- public/js/master/master.js
+++ public/js/master/master.js
@@ -6,58 +6,57 @@ $.ajaxSetup({
 });
 
 function addDataWithAjax(data_url,data_obj,error_msg_div,success_msg_div,reload_url,div_to_reload){
-$.ajax({
-    url: data_url,
-    type: 'POST',
-    dataType:'json',
-    data:data_obj,//data:{param:value,},
+    $.ajax({
+        url: data_url,
+        type: 'POST',
+        dataType:'json',
+data:data_obj,//data:{param:value,},
 enctype: 'multipart/form-data',
- processData: false,  // tell jQuery not to process the data
+processData: false,  // tell jQuery not to process the data
 contentType: false,   // tell jQuery not to set contentType
 })
-.done(function(response_data) {
-     modalDismiss();
-    if(response_data.length != 0 || response_data != null || response_data != "" ){
+    .done(function(response_data) {
+        modalDismiss();
+        if(response_data.length != 0 || response_data != null || response_data != "" ){
             if(response_data.errors){
-            $(error_msg_div).html('');
-            $.each(response_data.errors, function(key, value){
-                $(error_msg_div).show();
-                $(error_msg_div).append(`<li class="badge badge-danger">${value}</li>`);
-                dismiss_alert(error_msg_div);
-            });
+                $(error_msg_div).html('');
+                $.each(response_data.errors, function(key, value){
+                    $(error_msg_div).show();
+                    $(error_msg_div).append(`<li class="badge badge-danger">${value}</li>`);
+                    dismiss_alert(error_msg_div);
+                });
+            }else{
+                reload(reload_url,div_to_reload);
+                $(success_msg_div).html('');
+                $(success_msg_div).show();
+                $(success_msg_div).append(`<li class="badge badge-success">${response_data.success}</li>`);
+                dismiss_alert(success_msg_div);
+            } 
         }else{
-            reload(reload_url,div_to_reload);
-            $(success_msg_div).html('');
-            $(success_msg_div).show();
-            $(success_msg_div).append(`<li class="badge badge-success">${response_data.success}</li>`);
-            dismiss_alert(success_msg_div);
-        } 
-    }else{
-        console.log(response_data);
-    }
-})
-.fail(function() {
-    console.log("error");
-})
-.always(function() {
-    console.log("complete");
-});
+            console.log(response_data);
+        }
+    })
+    .fail(function() {
+        console.log("error");
+    })
+    .always(function() {
+        console.log("complete");
+    });
 }
 
 function editFormWithAjax(url_with_id,send_data_obj,modal_title){
-  $.ajax({
-      url: url_with_id,   // abc/id/edit
-      type: 'GET',
-     data:send_data_obj,
-  })
-  .done(function(response) {
+$.ajax({
+    url: url_with_id,   // abc/id/edit
+    type: 'GET',
+    data:send_data_obj,
+})
+.done(function(response) {
     if(response.success){
         modalInit(response.edit_html, modal_title);  
     }else{
-        modalInit(`<h6 class='alert alert-danger'>Something went wrong try Again</h6><hr />`,
-                 `Error <i class="fas fa-exclamation-triangle"></i>`);  
+        modalInit(`<h6 class='alert alert-danger'>Something went wrong try Again</h6><hr />`,`Error <i class="fas fa-exclamation-triangle"></i>`);  
     }
-  });
+});
 }
 
 
@@ -71,21 +70,24 @@ var delete_data = `
 </div>
 </div>`;
 
-function modalInit(dataToShow, title) {
+function modalInit(data, title) {
     $("#masterModal").modal({
-    backdrop: 'static',
-    keyboard: false
+        backdrop: 'static',
+        keyboard: false
     });
-    
     $("#modal-title").html(title);
-    document.querySelector('#modal-body').innerHTML = dataToShow; 
+    document.querySelector('#modal-body').innerHTML = data; 
 }
 function modalDismiss(){
-$('#masterModal').modal('toggle');
-$('.modal-backdrop').removeClass('modal-backdrop');
+    $('#masterModal').modal('toggle');
+    $('.modal-backdrop').removeClass('modal-backdrop');
 }
 
 function reload(url,response_div){
+    if(url.length == 0 || url == "" || response_div.length == 0 || response_div == "" response_div == "undefined"){
+        modalInit(`<h6 class='alert alert-danger'>URL and Response Div is required.!</h6><hr />`,`Error <i class="fas fa-exclamation-triangle"></i>`);  
+        return;
+    }
     $.ajax({
         url: url,
         type: 'post',
@@ -96,7 +98,7 @@ function reload(url,response_div){
 }
 
 function dismiss_alert(class_div_to_remove){
-        $(class_div_to_remove).fadeTo(2000, 500).slideUp(500, function() {
-      $(class_div_to_remove).slideUp(500);
+    $(class_div_to_remove).fadeTo(2000, 500).slideUp(500, function() {
+    $(class_div_to_remove).slideUp(500);
     });
 }
