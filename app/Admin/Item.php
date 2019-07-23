3a07ffb42deb537b388aa56d52a21a93d25f670b
Sadar Khan <sadarkhan@abtach.org>
2019-07-23 18:21:05 +0500

    change filter to guarded in item modal

--- app/Admin/Item.php
+++ app/Admin/Item.php
@@ -7,7 +7,7 @@ use App\Admin\ItemCategory;
 
 class Item extends Model
 {
-    protected $fillable = ['user_id','item_code_number','item_category_id','name','image_url','details'];
+    protected $guarded = [];
 
 public function item_category() {
 	return $this->belongsTo(ItemCategory::class);
